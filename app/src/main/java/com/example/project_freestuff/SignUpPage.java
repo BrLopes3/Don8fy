b0c51c83f9facdeae08905e7e04ad741bdf1da2c
Francisco Ricardo Andraschko <fricardo.it@gmail.com>
2024-03-07 09:22:42 -0500

    - Add firebase auth

--- app/src/main/java/com/example/project_freestuff/SignUpPage.java
+++ app/src/main/java/com/example/project_freestuff/SignUpPage.java
@@ -1,28 +1,41 @@
 package com.example.project_freestuff;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SignUpPage extends AppCompatActivity {
 
-    Button btncontinue, btnsignin;
-    EditText name, email, password;
+    Button btnregister, btnsignin;
+    EditText txtname, txtemail, txtpassword, txtconfirmpassword;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
 
-        btncontinue = findViewById(R.id.continuebtn);
+        btnregister = findViewById(R.id.registerbtn);
         btnsignin = findViewById(R.id.signInbtn);
+        mAuth = FirebaseAuth.getInstance();
 
-        name = findViewById(R.id.usernametxt);
-        email = findViewById(R.id.emailtxt);
-        password = findViewById(R.id.passwordtxt);
+        txtname = findViewById(R.id.usernametxt);
+        txtemail = findViewById(R.id.emailtxt);
+        txtpassword = findViewById(R.id.passwordtxt);
+        txtconfirmpassword = findViewById(R.id.confirmpasswordtxt);
 
         btnsignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,22 +45,58 @@ public class SignUpPage extends AppCompatActivity {
             }
         });
 
-        btncontinue.setOnClickListener(new View.OnClickListener() {
+        btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String useremail = email.getText().toString();
-                String userpassword = password.getText().toString();
-
-                Intent intent = new Intent(SignUpPage.this, LoginPage.class);
-                intent.putExtra("email", useremail);
-                intent.putExtra("password", userpassword);
-                startActivity(intent);
-
+                registerUser();
             }
         });
 
+    }
+    private void registerUser() {
+        String name = txtname.getText().toString().trim();
+        String email = txtemail.getText().toString().trim();
+        String password = txtpassword.getText().toString().trim();
+        String confirmPassword = txtconfirmpassword.getText().toString().trim();
 
 
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(this, "All the field should be filled!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.length() < 8) {
+            txtpassword.setError("Password should be more than 8 char");
+            txtpassword.requestFocus();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            txtconfirmpassword.setError("Password and confirmation is not match");
+            txtconfirmpassword.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            txtemail.setError("Please Put the valid Email address");
+            txtemail.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpPage.this,
+                                    "Register Success :) ", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpPage.this, LoginPage.class);
+                            intent.putExtra("email", email);
+                            intent.putExtra("password", password);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpPage.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
