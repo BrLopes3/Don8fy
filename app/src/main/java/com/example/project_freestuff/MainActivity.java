0c78b4a808321fe52f0ea4f2cc71bbc4ae400a3d
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2024-03-14 08:54:16 -0400

    start to code the DetailPage

--- app/src/main/java/com/example/project_freestuff/MainActivity.java
+++ app/src/main/java/com/example/project_freestuff/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.project_freestuff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridLayout gridProducts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridProducts = findViewById(R.id.gridLayoutProducts);
+
+        for (int i=0; i<gridProducts.getChildCount(); i++){
+            final View child = gridProducts.getChildAt(i);
+            final int index = i;
+            child.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(MainActivity.this, DetailPage.class);
+
+
+
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+
     }
 }
