cf0655b7cd0d9356cb60c08c4e7ae0d47b108555
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2024-03-15 10:12:24 -0400

    Add the ItemModel.java

---
+++ app/src/main/java/com/example/project_freestuff/ItemModel.java
@@ -0,0 +1,44 @@
+package com.example.project_freestuff;
+
+import android.content.ClipData;
+import android.widget.ImageView;
+
+public class ItemModel {
+
+  String name, desctiption;
+
+  ImageView picture;
+
+  //constructor
+  public ItemModel(String n, String d, ImageView img){
+      name = n;
+      desctiption = d;
+      picture = img;
+  }
+  //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesctiption() {
+        return desctiption;
+    }
+
+    public ImageView getPicture() {
+        return picture;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesctiption(String desctiption) {
+        this.desctiption = desctiption;
+    }
+
+    public void setPicture(ImageView picture) {
+        this.picture = picture;
+    }
+
+}
