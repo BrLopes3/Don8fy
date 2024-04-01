1deb986a3f4586c3bd90330bd74f0f813da76094
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2024-04-01 08:15:23 -0400

    Start to code the edit and delete operation for the item selected from the Recycler

--- app/src/main/java/com/example/project_freestuff/DetailPage.java
+++ app/src/main/java/com/example/project_freestuff/DetailPage.java
@@ -2,18 +2,31 @@ package com.example.project_freestuff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class DetailPage extends AppCompatActivity {
 
     ImageView productImage;
     TextView productName;
     TextView productDescription;
+
+    Button editItem, removeItem;
+
+    //references from the Firebase
+    private DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("items");
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference("images");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +41,31 @@ public class DetailPage extends AppCompatActivity {
         productName = findViewById(R.id.detailProductName);
         productDescription = findViewById(R.id.detailProductDescription);
         productImage = findViewById(R.id.imageProduct);
+        editItem = findViewById(R.id.btnEdit);
+        removeItem = findViewById(R.id.btnDelete);
 
         productName.setText(name);
         productDescription.setText(description);
 
         Glide.with(DetailPage.this).load(imageUri).into(productImage);
+
+        editItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailPage.this, EditItemPage.class);
+                startActivity(intent);
+                intent.putExtra("name", productName.toString());
+                intent.putExtra("description", productDescription.toString());
+
+            }
+        });
+
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
